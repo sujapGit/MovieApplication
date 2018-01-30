f9058b0cb0119b627867e1d5263d705e2be42860
sujapGit <sujap2011@gmail.com>
2018-01-30 14:29:50 -0500

    Added Site.css file for styling the website Added View for Search Movie , Side Navigation and Layout of the website Added Jquery Ajax method for listing genre , Searching movies and displaying the content

--- MovieApplication/Controllers/HomeController.cs
+++ MovieApplication/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,13 @@ namespace MovieApplication.Controllers
 {
     public class HomeController : Controller
     {
+        protected string dashboardUrlBase = "https://api.themoviedb.org/3/search/movie?api_key=aa646e218fda2ff0dfcd4d3a3cb55e37&language=en-US";
+
         public ActionResult Index()
         {
             return View();
         }
 
-      
+
     }
 }
---
+++ MovieApplication/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApplication.Controllers
+{
+    public class MovieController : Controller
+    {
+      
+    }
+}
