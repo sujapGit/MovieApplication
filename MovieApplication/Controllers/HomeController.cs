7d78c5637c233cfc58e9aebb71287faf333c9eb3
sujapGit <sujap2011@gmail.com>
2018-02-06 14:21:01 -0500

    Deleting unncessary Files , style , and components .. Finalizing the layout Adding Sorting Adding final Searching Working with Side navigation

--- MovieApplication/Controllers/HomeController.cs
+++ MovieApplication/Controllers/HomeController.cs
@@ -11,8 +11,7 @@ namespace MovieApplication.Controllers
 {
     public class HomeController : Controller
     {
-        protected string dashboardUrlBase = "https://api.themoviedb.org/3/search/movie?api_key=aa646e218fda2ff0dfcd4d3a3cb55e37&language=en-US";
-
+       
         public ActionResult Index()
         {
             return View();
